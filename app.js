5fc51ca0b585969791f5831ecd08faabd3974e61
HomameSoussi <121523106+HomameSoussi@users.noreply.github.com>
2023-04-14 16:19:53 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const searchForm = document.getElementById('search-form');
 const bookTitleInput = document.getElementById('book-title');
 const bookSummary = document.getElementById('book-summary');
-const loader = document.getElementById('loader');
 
 searchForm.addEventListener('submit', async (event) => {
   event.preventDefault();
@@ -9,30 +8,17 @@ searchForm.addEventListener('submit', async (event) => {
   const bookTitle = bookTitleInput.value.trim();
   if (!bookTitle) return;
 
-  loader.style.display = 'block';
-  bookSummary.innerHTML = '';
-  
-  try {
-    const summary = await getBookSummary(bookTitle);
-    bookSummary.innerHTML = summary;
-  } catch (error) {
-    console.error('Error fetching book summary:', error);
-    bookSummary.innerHTML = 'Error fetching book summary. Please try again later.';
-  } finally {
-    loader.style.display = 'none';
-  }
+  const summary = await getBookSummary(bookTitle);
+  bookSummary.innerHTML = summary;
 });
 
 async function getBookSummary(bookTitle) {
   try {
     const response = await fetch(`/api/getBookSummary?title=${encodeURIComponent(bookTitle)}`);
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
     const summary = await response.text();
     return summary;
   } catch (error) {
     console.error('Error fetching book summary:', error);
-    throw error;
+    return 'Error fetching book summary. Please try again later.';
   }
 }
